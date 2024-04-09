220ddeb416814cec88d50d778441ec465486d7d8
Ayush Gupta <ayushg1408@gmail.com>
2024-04-09 19:50:04 +0530

    FlatList and padiing with resizeMode of Image

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -19,6 +19,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
@@ -33,6 +34,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
